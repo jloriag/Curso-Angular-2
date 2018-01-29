8f2f77dff5c5a8a99132b7e674dd47e1c8791f8d
Josue <jloriag@hotmail.com>
2018-01-29 16:49:16 -0600

    Avances

--- Angular/1. Hola Mundo/src/app/app.module.ts
+++ Angular/1. Hola Mundo/src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 
 import {HeaderComponent} from "./components/header.component"
 
+import {BodyComponent} from "./components/body.component";
+import { FooterComponent } from './footer/footer.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
