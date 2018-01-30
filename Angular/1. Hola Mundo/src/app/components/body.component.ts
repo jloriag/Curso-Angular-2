ad05c27f2f9a938f5ad481ae7b554960a8d54ab1
Josue <jloriag@hotmail.com>
2018-01-30 09:26:18 -0600

    Directivas Estructurales

--- Angular/1. Hola Mundo/src/app/components/body.component.ts
+++ Angular/1. Hola Mundo/src/app/components/body.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: 'body.component.html'
 })
 export class BodyComponent {
-
+  mostrar:boolean=false;
+  frase:any={
+    mensaje:"Un gran poder requiere una gra responsabilidad",
+    autor:"Ben Parker"
+  }
+  personajes:string[]=["Spiderman","Venom","Dr Octopus"]
 }
